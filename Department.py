f8683faae50753f6d44a43670ae46a4ffb89328e
jnshort <justinnshort98@gmail.com>
2023-10-23 22:03:47 -0700

    fixed dept attributes

--- Department.py
+++ Department.py
@@ -12,10 +12,25 @@ class Department:
         self.office: int = office
         self.desc: str = desc
         self.db_connection = db
+        self.active: bool = False
 
 
-    def dict_repr(self):
-        dict = {
+
+    def constraints(self) -> bool:
+        """Checks uniqueness constraints of the Department class 
+        and that the departent does not already exist in the database.
+        Returns True if it passes.
+        :return:    Boolean
+        """
+        # todo
+        pass
+
+
+    def dict_repr(self) -> dict:
+        """Returns a dictionary representation of the class.
+        :return:    dict
+        """
+        dept = {
             "name": self.name,
             "abbreviation": self.abbreviation,
             "chair": self.chair,
@@ -23,28 +38,35 @@ class Department:
             "office": self.office,
             "description": self.description
         }
-        return dict
+        return dept
 
 
     def add_dept(self):
+        """Adds this department to the database.
+        :return:    None
+        """
         # todo
         pass
 
 
     def remove_dept(self):
+        """Removes this department from the database.
+        :return:    None"""
         # todo
         pass
 
 
     def list_dept(self):
+        """Lists the departments that are in the database.
+        :return:    String"""
         # todo
         pass
 
 
     def __str__(self):
         text = f"Name: {self.name} Abbreviation: {self.abbreviation}\n"
-        text += f"Chair: {self.chair} Building: {self.building} Office: {self.office}\n"
-        text += f"Description: {self.description}"
+        text += f"\tChair: {self.chair} Building: {self.building} Office: {self.office}\n"
+        text += f"\tDescription: {self.description}"
         return text
 
 
