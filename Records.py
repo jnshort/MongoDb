447c752f17dc5037891dd738248496b23a8beae4
jnshort <131628097+jnshort@users.noreply.github.com>
2023-10-24 14:52:31 -0700

    Update Records.py

--- Records.py
+++ Records.py
@@ -5,29 +5,29 @@ from Department import Department
 
 
 class Records:
-    """A singleton class used to keep record of what we have
-    added to the database, in addition to storing the connection
-    to the database to be accessed from anywhere in the program.
-    """
-    _instance = None
-    _initialized = False
+	"""A singleton class used to keep record of what we have
+	added to the database, in addition to storing the connection
+	to the database to be accessed from anywhere in the program.
+	"""
+	_instance = None
+	_initialized = False
 
-    def __new__(cls, *args):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls.instance
+	def __new__(cls, *args):
+		if cls._instance is None:
+			cls._instance = super().__new__(cls)
+		return cls.instance
             
 
-    def __init__(self):
+	def __init__(self):
 		if not Records._initialized:
 			self.db_connect = MongoClient(db)
-           	self.departments = [] # im not sure why the indentation on this line is messed up, it shows correctly on my ide but not in github
+           		self.departments = [] # im not sure why the indentation on this line is messed up, it shows correctly on my ide but not in github
 			Singleton._initialized = True
 
 
-    def new_dept_rec(self, dept: Department):
-        self.departments.append(dept)
+	def new_dept_rec(self, dept: Department):
+        	self.departments.append(dept)
 
 
-    def remove_dept_rec(self, dept: Department):
-        self.departments.remove(dept)
+	def remove_dept_rec(self, dept: Department):
+		self.departments.remove(dept)
