e89fb7c0a604b83ca870eea1711ff1a47cdc2ebf
HeatherHawkins <58083957+HeatherHawkins@users.noreply.github.com>
2023-12-09 13:16:21 -0800

    Add files via upload

---
+++ Course.py
@@ -0,0 +1,52 @@
+import pymongo
+from pymongo import MongoClient
+from db import db
+from Records import Records
+
+class Course:
+
+    def __init__(self, course_number: int, course_name: str, description: str, units: int):
+        self.course_number: int = course_number
+        self.course_name: str = course_name
+        self.description: str = description
+        self.units: int = units
+        self.db = MongoClient(db)
+        self.active: bool = False
+
+    def checkFields(self) -> bool:
+        return (
+            len(self.course_number) <= 700 and
+            len(self.units) <= 5)
+
+    def dict_repr(self) -> dict:
+        """Returns a dictionary representation of the class.
+        :return:    dict
+        """
+        course = {
+            "course_number": self.course_number,
+            "course_name": self.course_name,
+            "description": self.description,
+            "units": self.units,
+        }
+        return course
+
+    def add_dept(self):
+        """Adds this course to the database and the
+        records list of courses.
+        :return:    None
+        """
+
+        rec = Records()
+        rec.courses.insert_one(self.dict_repr())
+        self.active = True
+
+    def remove_dept(self):
+        """Removes this course from the database.
+        Main has already verified that this course is in the database.
+        :return:    None
+        """
+        rec = Records()
+        self.active = False
+
+        #to_del = rec.db_connect.singlecollection.departments.find()
+        rec.courses.delete_one({"course_name":self.course_name})
---
+++ Section.py
@@ -0,0 +1,59 @@
+import pymongo
+from pymongo import MongoClient
+from db import db
+from Records import Records
+
+class Section:
+
+    def __init__(self, section_number: int, semester: str, section_year: str, building: str, room: int, schedule: str, start_time: str, instructor: str):
+        self.section_number: int = section_number
+        self.semester: str = semester
+        self.section_year: str = section_year
+        self.building: str = building
+        self.room: int = room
+        self.schedule: str = schedule
+        self.start_time: str = start_time
+        self.instructor: str = instructor
+        self.db = MongoClient(db)
+        self.active: bool = False
+
+    def checkFields(self) -> bool:
+        return (
+            len(self.room) <= 1000)
+
+    def dict_repr(self) -> dict:
+        """Returns a dictionary representation of the class.
+        :return:    dict
+        """
+        section = {
+            "section_number": self.section_number,
+            "semester": self.semester,
+            "section_year": self.section_year,
+            "building": self.building,
+            "room": self.room,
+            "schedule": self.schedule,
+            "start_time": self.start_time,
+            "instructor": self.instructor
+        }
+        return section
+
+    def add_dept(self):
+        """Adds this section to the database and the
+        records list of sections.
+        :return:    None
+        """
+
+        rec = Records()
+        rec.sections.insert_one(self.dict_repr())
+        self.active = True
+
+    def remove_dept(self):
+        """Removes this section from the database.
+        Main has already verified that this section is in the database.
+        :return:    None
+        """
+        rec = Records()
+        self.active = False
+
+        #to_del = rec.db_connect.singlecollection.departments.find()
+        rec.sections.delete_one({"section_number":self.section_number})
